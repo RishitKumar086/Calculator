786782df01ac5791e41f4d8dec1dce363282ccc9
Rishit <rishit1910kumar@gmail.com>
2023-08-15 12:13:45 +0530

    thirteenth

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 const number = document.querySelectorAll("#num");
+const operator = document.querySelectorAll("#operator");
+const display = document.querySelector("#display");
+console.log(number);
+console.log(operator);
+console.dir(display);
 for (let num of number) {
   num.addEventListener("click", function () {
     console.log("Number button pressed");
+    // console.log(`Number ${num.innerHTML}`);
+    // console.log(typeof num.innerHTML);
+    // num.innerHTML is a string
+    // display.innerHTML=12
+    if (display.innerHTML == "0") {
+      display.innerHTML = "";
+    }
+    display.innerHTML += num.innerHTML;
   });
 }
+for (let opt of operator) {
+  opt.addEventListener("click", function () {
+    console.log("Operator button pressed");
+  });
+}
+
+display.addEventListener("click", function () {
+  console.log("display clicked");
+  console.log(typeof display.innerHTML);
+});
