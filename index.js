c378928e784b8dcb0da8715f4f623c842ba899cc
Rishit <rishit1910kumar@gmail.com>
2023-08-21 15:29:39 +0530

    15th

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const number = document.querySelectorAll("#num");
 const operator = document.querySelectorAll("#operator");
 const display = document.querySelector("#display");
-console.log(number);
-console.log(operator);
-console.dir(display);
+let op;
+let n1;
+let n2;
+
+// console.log(number);
+// console.log(operator);
+// console.dir(display);
 
 for (let num of number) {
   num.addEventListener("click", function () {
-    console.log("Number button pressed");
+    // console.log("Number button pressed");
     // console.log(`Number ${num.innerHTML}`);
     // console.log(typeof num.innerHTML);
     // num.innerHTML is a string
@@ -21,17 +25,51 @@ for (let num of number) {
 
 for (let opt of operator) {
   opt.addEventListener("click", function () {
-    console.log("Operator button pressed");
+    // console.log("Operator button pressed");
     // console.log(opt.dataset.action);
     const action = opt.dataset.action;
     // console.log(typeof action);
+
     if (action === "allclear") {
       display.innerHTML = "0";
+      n1 = 0;
+      n2 = 0;
+    }
+    if (action === "decimal") {
+      display.innerHTML += ".";
+    }
+    if (
+      action === "add" ||
+      action === "sub" ||
+      action === "mul" ||
+      action === "div"
+    ) {
+      op = action;
+      // console.log(op);
+      n1 = Number.parseFloat(display.innerHTML);
+      display.innerHTML = "";
+    }
+    if (action === "equals") {
+      // console.log(n1);
+      n2 = Number.parseFloat(display.innerHTML);
+      // console.log(n2);
+      if (op === "add") {
+        display.innerHTML = n1 + n2;
+      } else if (op === "sub") {
+        display.innerHTML = n1 - n2;
+      } else if (op === "mul") {
+        display.innerHTML = n1 * n2;
+      } else {
+        display.innerHTML = n1 / n2;
+      }
+    }
+    if ((action = "delete")) {
+      
     }
   });
 }
 
-display.addEventListener("click", function () {
-  console.log("display clicked");
-  console.log(typeof display.innerHTML);
-});
+// display.addEventListener("click", function () {
+//   console.log("display clicked");
+//   console.log(typeof display.innerHTML);
+// });
